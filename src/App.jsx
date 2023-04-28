9810844d482cf8cbf19b8b02e6e995e8f40670ef
Evaristo Perez <perezlopezevaristo07@outlook.com>
2023-04-27 19:56:42 -0500

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+
+function App() {
+  return (
+    <header>
+      <div className="brand"></div>
+      <ul className="nav">
+        <li className="nav__item">How it Works</li>
+        <li className="nav__item">Downloads</li>
+        <li className="nav__item">Support</li>
+        <li className="nav__item">Shop</li>
+      </ul>
+    </header>
+  );
+}
+
+export default App;
